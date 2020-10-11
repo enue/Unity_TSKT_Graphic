e55838ee4d42c22f6746f9c66fbc75175b6b8bf8
enu <otoriden@gmail.com>
2020-10-11 15:37:07 +0900

    TextureUtil.Resize

---
+++ Assets/Package/Runtime/TextureUtil.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TSKT
+{
+    public static class TextureUtil
+    {
+        static public void Resize(ref Texture2D texture, int width, int height)
+        {
+            var rt = RenderTexture.GetTemporary(width, height);
+            Graphics.Blit(texture, rt);
+            RenderTexture.active = rt;
+
+            texture.Resize(width, height);
+            texture.ReadPixels(new Rect(0, 0, texture.width, texture.height), 0, 0);
+            texture.Apply();
+
+            RenderTexture.ReleaseTemporary(rt);
+            RenderTexture.active = null;
+        }
+    }
+}
