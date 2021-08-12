982e590299ead4f4e582275e963fe97de3e5a4cf
enu <otoriden@gmail.com>
2021-08-12 11:27:48 +0900

    unity2021.1.16

--- Assets/Samples/CaptureScreenshotSample.cs
+++ Assets/Samples/CaptureScreenshotSample.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cysharp.Threading.Tasks;
 
 namespace TSKT
 {
@@ -12,6 +13,14 @@ namespace TSKT
             yield return TextureUtil.CaptureScreenshot(100, 100, _ => texture = _);
             var bytes = texture.EncodeToJPG();
             System.IO.File.WriteAllBytes("ss.jpg", bytes);
+            A();
+        }
+
+        async void A()
+        {
+            var texture = await TextureUtil.CaptureScreenshot(100, 100, this);
+            var bytes = texture.EncodeToJPG();
+            System.IO.File.WriteAllBytes("ss2.jpg", bytes);
         }
     }
 }
