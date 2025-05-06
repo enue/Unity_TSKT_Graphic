f20bbd4acc2bc7638aa7d390692fbfcf3c0720d0
enu <otoriden@gmail.com>
2025-05-06 20:13:11 +0900

    convert texture into base64

--- Assets/Package/Runtime/TextureUtil.cs
+++ Assets/Package/Runtime/TextureUtil.cs
@@ -1,4 +1,5 @@
 ﻿#nullable enable
+using System.Buffers.Text;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,7 @@ namespace TSKT
 {
     public static class TextureUtil
     {
-        static public void Resize(ref Texture2D texture, int width, int height)
+        public static void Resize(ref Texture2D texture, int width, int height)
         {
             var rt = RenderTexture.GetTemporary(width, height);
             Graphics.Blit(texture, rt);
@@ -42,6 +43,45 @@ namespace TSKT
             }
 
             return texture;
+        }
+
+        public static Vector2 Shrink(float aspectWidth, float aspectHeight, float width, float height)
+        {
+            if (aspectHeight * width > aspectWidth * height)
+            {
+                return new Vector2(height * aspectWidth / aspectHeight, height);
+            }
+            else
+            {
+                return new Vector2(width, width * aspectHeight / aspectWidth);
+            }
+        }
+
+        public static string ToBase64(byte[] bytes)
+        {
+            System.Span<byte> buffer = stackalloc byte[Base64.GetMaxEncodedToUtf8Length(bytes.Length)];
+            Base64.EncodeToUtf8(bytes, buffer, out _, out var bytesWritten);
+            return System.Text.Encoding.UTF8.GetString(buffer[..bytesWritten]);
+        }
+
+        public static int GetMaxByteCount(int base64StringLength)
+        {
+            var maxByteCount = System.Text.Encoding.UTF8.GetMaxByteCount(base64StringLength);
+            return Base64.GetMaxDecodedFromUtf8Length(maxByteCount);
+        }
+
+        public static void FromBase64(string base64, System.Span<byte> dest, out int writtenCount)
+        {
+            System.Span<byte> utf8 = stackalloc byte[System.Text.Encoding.UTF8.GetMaxByteCount(base64.Length)];
+            var utf8Length = System.Text.Encoding.UTF8.GetBytes(base64, utf8);
+            Base64.DecodeFromUtf8(utf8[..utf8Length], dest, out _, out writtenCount);
+        }
+
+        public static void FromBase64(string base64, Texture2D dest)
+        {
+            System.Span<byte> bytes = stackalloc byte[GetMaxByteCount(base64.Length)];
+            FromBase64(base64, bytes, out var writtenCount);
+            dest.LoadImage(bytes[..writtenCount], markNonReadable: true);
         }
     }
 }
